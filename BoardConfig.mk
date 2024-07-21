0297ce43b87f3ca7d7366f7f287c555501b69acc
Ricky Cheung <rcheung844@gmail.com>
2024-07-21 16:57:24 +0800

    A57: Enable USB ConfigFS for kernel 4.9
    Change-Id: Ifdc714d2e65d003aa3d51b6a92d5921456f18bd7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,6 +48,7 @@ BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.usbconfigfs=true
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET := 0x1000000
